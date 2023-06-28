802743acc1c1657d560916af7e8be95f093017b4
Schmidt Rafael <48494280+schmidtrafael@users.noreply.github.com>
2023-06-28 15:54:12 -0300

    Update multiplos.java

--- curso_java/src/if_else/multiplos.java
+++ curso_java/src/if_else/multiplos.java
@@ -1,10 +1,24 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class multiplos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A % B == 0 || B % A ==0) {
+			System.out.println("São Multiplos");
+		}
+		else {
+			System.out.println("Não são Multiplos!");
+		}
+		
+		sc.close();
 	}
 
 }
