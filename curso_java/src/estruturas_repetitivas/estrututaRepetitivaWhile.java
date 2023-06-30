4e83fc547434c3c9d25e37ae4536a4920089e995
Schmidt Rafael <rsn.schmidt@gmail.com>
2023-06-29 22:40:24 -0300

    create exemplo de estrutura repetitica java
    usando estrutura simples de whille

---
+++ curso_java/src/estruturas_repetitivas/estrututaRepetitivaWhile.java
@@ -0,0 +1,23 @@
+package estruturas_repetitivas;
+
+import java.util.Scanner;
+
+public class estrututaRepetitivaWhile {
+    
+    public static void main(String[] agrs) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int soma = 0;
+        while (x != 0) {
+            
+            soma += x;
+            x = sc.nextInt();
+        }
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
