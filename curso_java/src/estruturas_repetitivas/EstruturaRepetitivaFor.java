edc465e0e63000a1bceb094687475f38ba626827
Schmidt Rafael <rsn.schmidt@gmail.com>
2023-07-04 22:53:03 -0300

    Update EstruturaRepetitivaFor.java

--- curso_java/src/estruturas_repetitivas/EstruturaRepetitivaFor.java
+++ curso_java/src/estruturas_repetitivas/EstruturaRepetitivaFor.java
@@ -8,18 +8,19 @@ public class EstruturaRepetitivaFor {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Quantos numeros vamos somar?");
         int N = sc.nextInt();
 
         int soma = 0;
 
         for (int i=0; i<N; i++) {
-
+            System.out.println("Insira mais um numero!");
             int x = sc.nextInt();
             soma = soma + x;
 
         }
 
-        System.out.println(soma);
+        System.out.printf("A soma total dos numeros inseridos = %d \n", soma);
 
         sc.close();
     }
