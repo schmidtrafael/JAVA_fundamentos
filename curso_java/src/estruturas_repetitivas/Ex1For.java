7d3014bdb3a1b4ca5bb0b03bb7d9911f6178f672
Schmidt Rafael <rsn.schmidt@gmail.com>
2023-07-05 16:50:31 -0300

    ex1For
    Lendo um valor na variavel X e imprimindo todos os valores impares até o X

---
+++ curso_java/src/estruturas_repetitivas/Ex1For.java
@@ -0,0 +1,26 @@
+package estruturas_repetitivas;
+
+import java.util.Scanner;
+
+public class Ex1For {
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+
+        for (int i=1; i<=x; i++){
+        
+            if (i % 2 != 0){
+                System.out.println(i);
+        
+            }
+
+        }
+
+        sc.close();
+
+    }
+
+}
